3e739dfe92c8ff4a165362874c0b1060dcb2c20b
AI1411 <goethe0719@gmail.com>
2020-11-07 11:26:22 +0900

    add column to articles

--- db/migrations/20201106234759_create_articles.sql
+++ db/migrations/20201106234759_create_articles.sql
@@ -2,7 +2,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 CREATE TABLE articles (
-    id int AUTO_INCREMENTl,
+    id int AUTO_INCREMENT,
     title varchar(100),
     PRIMARY KEY(id)
 );
